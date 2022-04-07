670eb8d7b69a5e9defdab4b20bc7e324fa7ec24b
priyanka <priyankavundi725@gmail.com>
2022-04-07 14:08:35 +0530

    commit

---
+++ index.js
@@ -0,0 +1,112 @@
+function myFunction1() {
+    var a = document.getElementById("button").value;
+    document.getElementById("grid1").innerHTML = a;
+}
+function myFunction2() {
+    var b = document.getElementById("button").value;
+    document.getElementById("grid2").innerHTML = b;
+}
+function myFunction3() {
+    var c = document.getElementById("button").value;
+    document.getElementById("grid3").innerHTML = c;
+}
+function myFunction4() {
+    var d = document.getElementById("button").value;
+    document.getElementById("grid4").innerHTML = d;
+}
+function myFunction5() {
+    var e = document.getElementById("button").value;
+    document.getElementById("grid5").innerHTML = e;
+}
+function myFunction6() {
+    var f = document.getElementById("button").value;
+    document.getElementById("grid6").innerHTML = f;
+}
+function myFunction7() {
+    var g = document.getElementById("button").value;
+    document.getElementById("grid7").innerHTML = g;
+}
+function myFunction8() {
+    var h = document.getElementById("button").value;
+    document.getElementById("grid8").innerHTML = h;
+}
+function myFunction9() {
+    var i = document.getElementById("button").value;
+    document.getElementById("grid9").innerHTML = i;
+}
+function myFunction10() {
+    var j = document.getElementById("button").value;
+    document.getElementById("grid10").innerHTML = j;
+}
+function myFunction11() {
+    var k = document.getElementById("button").value;
+    document.getElementById("grid11").innerHTML = k;
+}
+function myFunction12() {
+    var l = document.getElementById("button").value;
+    document.getElementById("grid12").innerHTML = l;
+}
+function myFunction13() {
+    var m = document.getElementById("button").value;
+    document.getElementById("grid13").innerHTML = m;
+}
+function myFunction14() {
+    var n = document.getElementById("button").value;
+    document.getElementById("grid14").innerHTML = n;
+}
+function myFunction15() {
+    var o = document.getElementById("button").value;
+    document.getElementById("grid15").innerHTML = o;
+}
+function myFunction16() {
+    var p = document.getElementById("button").value;
+    document.getElementById("grid16").innerHTML = p;
+}
+function myFunction17() {
+    var q = document.getElementById("button").value;
+    document.getElementById("grid17").innerHTML = q;
+}
+function myFunction18() {
+    var r = document.getElementById("button").value;
+    document.getElementById("grid18").innerHTML = r;
+}
+function myFunction19() {
+    var s = document.getElementById("button").value;
+    document.getElementById("grid19").innerHTML = s;
+}
+function myFunction20() {
+    var t = document.getElementById("button").value;
+    document.getElementById("grid20").innerHTML = t;
+}
+function myFunction21() {
+    var u = document.getElementById("button").value;
+    document.getElementById("grid21").innerHTML = u;
+}
+function myFunction22() {
+    var v = document.getElementById("button").value;
+    document.getElementById("grid22").innerHTML = v;
+}
+function myFunction23() {
+    var w = document.getElementById("button").value;
+    document.getElementById("grid23").innerHTML = w;
+}
+function myFunction24() {
+    var x = document.getElementById("button").value;
+    document.getElementById("grid24").innerHTML = x;
+}
+function myFunction25() {
+    var y = document.getElementById("button").value;
+    document.getElementById("grid25").innerHTML = y;
+}
+function myFunction26() {
+    var z = document.getElementById("button").value;
+    document.getElementById("grid26").innerHTML = z;
+}
+function myFunction27() {
+    var A = document.getElementById("button").value;
+    document.getElementById("grid27").innerHTML = A;
+}
+function myFunction28() {
+    var B = document.getElementById("button").value;
+    document.getElementById("grid28").innerHTML = B;
+}
